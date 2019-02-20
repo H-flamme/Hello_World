18872ef7cf59c894f9579e3642118772363b0534
Verigyk <lygregory@hotmail.fr>
2019-02-20 11:16:26 +0100

    Piano.py et dossier sounds

--- Piano.py
+++ Piano.py
@@ -1,8 +1,14 @@
-WIDTH = 734
+WIDTH = 735
 HEIGHT = 99
-for valeur in range(5):
-    do + valeur = Actor('do_0')
-do = [do_0,]
+
+touche = Actor('do_diese_1')
+touche.pos = 10,10
+
 def draw():
     screen.fill((254,254,254))
-    screen.blit("do_diese_2",(0,0))
+    touche.draw()
+
+def on_mouse_down(pos):
+    if touche.collidepoint(pos):
+        print("touché")
+        sounds.eep.play()
