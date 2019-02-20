b5f6a236bcf18fbc463773bfe19acee7d34d0dc1
Verigyk <lygregory@hotmail.fr>
2019-02-20 12:55:16 +0100

    Piano.py

--- Piano.py
+++ Piano.py
@@ -1,14 +1,19 @@
 WIDTH = 735
 HEIGHT = 99
 
-touche = Actor('do_diese_1')
-touche.pos = 10,10
+do = [Actor('do_0'),Actor('la_0')]
+do_diese = Actor('do_diese_1')
+sound = [str('eep'),str('eep')]
 
 def draw():
     screen.fill((254,254,254))
-    touche.draw()
+    for valeur in range(2):
+        do[valeur].pos = 10 + valeur * 21,50
+        do[valeur].draw()
 
 def on_mouse_down(pos):
-    if touche.collidepoint(pos):
-        print("touché")
-        sounds.eep.play()
+    for valeur in range(2):
+        if do[valeur].collidepoint(pos):
+            print("touché")
+            str truc = sound[valeur]
+            sounds.truc.play()
