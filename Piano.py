7df32f57efcc1cfb55217a07dd82c665a7db3cc8
Verigyk <lygregory@hotmail.fr>
2019-02-20 22:21:49 +0100

    Gros changement
    Dossier sounds et piano.py

--- Piano.py
+++ Piano.py
@@ -8,12 +8,19 @@ Actor('do_3'),Actor('la_0'),Actor('mi_0'),Actor('fa_0'),Actor('la_0'),Actor('la_
 Actor('do_4'),Actor('la_0'),Actor('mi_0'),Actor('fa_0'),Actor('la_0'),Actor('la_0'),Actor('mi_0'),
 Actor('la_0'),Actor('mi_0'),Actor('fa_0')]
 do_diese = []
+do_diese1 = Actor('do_diese_1')
 
 notes = [sounds.a_1,sounds.a_2,sounds.a_3,sounds.a_4,sounds.a_5,sounds.a_6,sounds.a_7,
 sounds.b_1,sounds.b_2,sounds.b_3,sounds.b_4,sounds.b_5,sounds.b_6,sounds.b_7,
 sounds.c_1,sounds.c_2,sounds.c_3,sounds.c_4,sounds.c_5,sounds.c_6,sounds.c_7,
 sounds.d_1,sounds.d_2,sounds.d_3,sounds.d_4,sounds.d_5,sounds.d_6,sounds.d_7,
 sounds.e_1,sounds.e_2,sounds.e_3,sounds.e_4,sounds.e_5,sounds.e_6,sounds.e_7]
+
+notes_diese = [sounds.a_1_5,sounds.a_2_5,sounds.a_4_5,sounds.a_5_5,sounds.a_6_5,
+sounds.b_1_5,sounds.b_2_5,sounds.b_4_5,sounds.b_5_5,sounds.b_6_5,
+sounds.c_1_5,sounds.c_2_5,sounds.c_4_5,sounds.c_5_5,sounds.c_6_5,
+sounds.d_1_5,sounds.d_2_5,sounds.d_4_5,sounds.d_5_5,sounds.d_6_5,
+sounds.e_1_5,sounds.e_2_5,sounds.e_4_5,sounds.e_5_5,sounds.e_6_5,]
 
 def draw():
     place = 0
@@ -34,12 +41,12 @@ def draw():
             place += 1
 def on_mouse_down(pos):
     musics = 0
-    for valeur in range(25):
-        if do_diese[valeur].collidepoint(pos):
-            musics = 0
-        else:
+    for valeur2 in range(25):
+        if do_diese[valeur2].collidepoint(pos):
+            notes_diese[valeur2].play()
             musics = 1
-    if musics == 1:
+            break
+    if musics == 0:
         for valeur in range(35):
             if do0[valeur].collidepoint(pos):
                 notes[valeur].play()
